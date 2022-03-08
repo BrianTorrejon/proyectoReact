bc33e1bdca59ad2721f295029ca420ff3f6fcf94
Jonatan <jonatanchopp22@gmail.com>
2022-03-07 22:49:41 -0300

    Ubicacion de Elementos

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+function App() {
+    return (
+          <div>
+              <div>
+              {/* logo */}
+              </div>
+              <div>
+               {/* Drop Dows */}
+              </div>
+              <div>
+               {/* iconos */}
+              </div>
+          </div>
+    );
+  }
