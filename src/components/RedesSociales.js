d6c436a15840deefd1337d0a55aecf79792a929f
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-17 16:57:22 -0300

    Se agregaron iconos y media query para resposividad

--- src/components/RedesSociales.js
+++ src/components/RedesSociales.js
@@ -1,34 +1,61 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import Tarjeta from './Tarjeta';
+import { Container, IconButton, Grid, Box } from "@material-ui/core";
+import facebook from '../icons/facebook.png'
+import ig from '../icons/instagram.png'
+import lin from '../icons/linkedin.png'
+import mail from '../icons/mail.png'
+import twitter from '../icons/twitter.png'
+import wp from '../icons/whatsapp.png'
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 
-export const ContenedorOfertas = () => {
+export const RedesSociales = () => {
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+
+
   return (
     <>
-    <Container>
-      <Grid component="h1" align="center">Ultimas <span>ofertas</span></Grid>
-      
-        <Grid container spacing={3} align="center">
-          <Grid item xs={12} md={6} lg={3}>
-            <Tarjeta />
+      <Box
+        display="flex"
+        height={260}
+        bgcolor="lightgreen"
+        alignItems="center"
+        justifyContent="center">
+        <Container>
+          <Grid component="h1" align="center">Compartelo con un amigo</Grid>
+          {console.log(matches)}
+
+          <Grid container align="center">
+            {console.log(theme.breakpoints.up('sm'))}
+            {matches ?
+              <Grid item sm={12} md={12} lg={12}>
+                <IconButton><img src={facebook} alt="facebook" /></IconButton>
+                <IconButton><img alt="instagram" src={ig} /></IconButton>
+                <IconButton><img src={lin} alt='linkedin' /></IconButton>
+                <IconButton><img src={mail} alt='mail' /></IconButton>
+                <IconButton><img src={twitter} alt='twitter' /></IconButton>
+                <IconButton><img src={wp} alt='whatsapp' /></IconButton>
+              </Grid>
+              :
+              <><Grid item xs={12}>
+                <IconButton><img src={facebook} alt="facebook" /></IconButton>
+                <IconButton><img alt="instagram" src={ig} /></IconButton>
+                <IconButton><img src={lin} alt='linkedin' /></IconButton>
+              </Grid>
+              <Grid item xs={12}>
+                  <IconButton><img src={mail} alt='mail' /></IconButton>
+                  <IconButton><img src={twitter} alt='twitter' /></IconButton>
+                  <IconButton><img src={wp} alt='whatsapp' /></IconButton>
+                </Grid></>
+            }
           </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <Tarjeta />
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <Tarjeta />
-          </Grid>
-          <Grid item xs={12} md={6} lg={3}>
-            <Tarjeta />
-          </Grid>
-        </Grid>
         </Container>
-    
+      </Box >
 
     </>
   )
 }
 
-export default ContenedorOfertas;
+export default RedesSociales;
