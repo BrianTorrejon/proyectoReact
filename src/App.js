8f75ac257fffd4b5304ba51957a54a11b5423cb4
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-19 15:36:33 -0300

    Se corrigen estilos de iconos y color de contenedor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import * as React from 'react';
 import {RedesSociales} from './components/RedesSociales'
-
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <div>
+      <CssBaseline />
         <RedesSociales/>
         </div>
   
--- src/components/RedesSociales.js
+++ src/components/RedesSociales.js
@@ -1,10 +1,10 @@
 import { Container, IconButton, Grid, Box } from "@material-ui/core";
-import facebook from '../icons/facebook.png'
-import ig from '../icons/instagram.png'
-import lin from '../icons/linkedin.png'
-import mail from '../icons/mail.png'
-import twitter from '../icons/twitter.png'
-import wp from '../icons/whatsapp.png'
+import Facebook from '@mui/icons-material/FacebookRounded';
+import Whatsapp from '@mui/icons-material/WhatsappRounded';
+import Instagram from '@mui/icons-material/Instagram';
+import Telegram from '@mui/icons-material/Telegram';
+import Twitter from '@mui/icons-material/Twitter';
+import Email from '@mui/icons-material/EmailRounded';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
@@ -17,43 +17,42 @@ export const RedesSociales = () => {
 
   return (
     <>
-      <Box
+      <Box //style={{backgroundImage: 'url(https://static.vecteezy.com/system/resources/previews/003/536/679/non_2x/light-purple-background-with-music-symbols-vector.jpg)',
+        //  backgroundRepeat: 'no-repeat',
+        //  backgroundSize: 'cover',
+        //  backgroundPosition: 'center'}}
         display="flex"
-        height={260}
-        bgcolor="lightgreen"
+        minHeight={260}
+        bgcolor="#D4D1FC"
         alignItems="center"
         justifyContent="center">
         <Container>
           <Grid component="h1" align="center">Compartelo con un amigo</Grid>
-          {console.log(matches)}
-
           <Grid container align="center">
-            {console.log(theme.breakpoints.up('sm'))}
             {matches ?
               <Grid item sm={12} md={12} lg={12}>
-                <IconButton><img src={facebook} alt="facebook" /></IconButton>
-                <IconButton><img alt="instagram" src={ig} /></IconButton>
-                <IconButton><img src={lin} alt='linkedin' /></IconButton>
-                <IconButton><img src={mail} alt='mail' /></IconButton>
-                <IconButton><img src={twitter} alt='twitter' /></IconButton>
-                <IconButton><img src={wp} alt='whatsapp' /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Facebook style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Whatsapp style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Telegram style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Twitter style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Email style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Instagram style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
               </Grid>
               :
               <><Grid item xs={12}>
-                <IconButton><img src={facebook} alt="facebook" /></IconButton>
-                <IconButton><img alt="instagram" src={ig} /></IconButton>
-                <IconButton><img src={lin} alt='linkedin' /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Facebook style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Whatsapp style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Telegram style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
               </Grid>
-              <Grid item xs={12}>
-                  <IconButton><img src={mail} alt='mail' /></IconButton>
-                  <IconButton><img src={twitter} alt='twitter' /></IconButton>
-                  <IconButton><img src={wp} alt='whatsapp' /></IconButton>
+                <Grid item xs={12}>
+                  <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Twitter style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                  <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Email style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
+                  <IconButton style={{ backgroundColor: "#503BD9", margin: 10, width: 60, height: 60 }}><Instagram style={{ color: '#D4D1FC', fontSize: '42' }} /></IconButton>
                 </Grid></>
             }
           </Grid>
         </Container>
       </Box >
-
     </>
   )
 }
