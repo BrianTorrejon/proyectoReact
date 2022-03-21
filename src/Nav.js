ab8107dd24c0239cd418ad61842b6be747e56746
Jonatan <jonatanchopp22@gmail.com>
2022-03-21 18:01:09 -0300

    Se agrego Nuevos Menus, Logo-Marca y Iconos

--- src/Nav.js
+++ src/Nav.js
@@ -1,30 +1,62 @@
-import React from 'react';
-import {Appbar, Menu, MenuList, Toolbar, Box, Button, IconButton} from '@mui/material';
-import {link} from 'react-router-dom';
+import React, {useState,} from 'react';
+import {AppBar, Menu, MenuList, Toolbar, Box, BottomNavigation, IconButton} from '@mui/material';
+
 
 
 
 function App() {
-    <>
-    const LinkButton =(props) => <Button color='primary' component={link} {...props} /> 
+    const [dropdown, setDropdown]=useState(false);
+
+    const abrirCerrarDropdown=()=>{
+      setDropdown (!dropdown);
+    }
+  
+    const LinkButton = (props) => <BottomNavigation color='primary' isOpen={dropdown} toggle={abrirCerrarDropdown} bool={true} {...props} /> 
     return (
-        <Appbar position= 'fixed'>
+        <AppBar position= 'fixed'>
             <Toolbar>
                 <Box>
-                {/* logo */}
+                <img src='C:\Users\Jonatan\proyectoReact\public\speaker-with-three-sound-waves-sound-emoji-11563014782dp23c5uqq3.png'alt='Simbolo Marca' ></img>
+                <h1>Sound On</h1>  
                 </Box>
                 <Box>
                 {/* Drop Dows */}
-                <Menu>
+    
                 <IconButton edge='start' color= 'inherit' arial-label= 'menu' ></IconButton>
-               
+               <Menu>
                 <LinkButton to='/'>
                     Home
                 </LinkButton>
-                <LinkButton to='/categories'>
-                    Categories
-                </LinkButton>
                 </Menu>
+
+                <Menu>
+                <LinkButton to='/Categories'>
+                    Categorias
+                </LinkButton>
+                <MenuList>Guitarras/Bajos</MenuList>
+                <MenuList>Baterias</MenuList>
+                <MenuList>Piano</MenuList>
+                <MenuList>Violines</MenuList>
+                </Menu>
+
+                <Menu>
+                <LinkButton to='/Sobre Nosotros'>
+                    Sobre Nosotros
+                </LinkButton>
+                <MenuList>Historia</MenuList>
+                <MenuList>Contacto</MenuList>
+                </Menu>
+
+                <Menu>
+                <LinkButton to='/Mi Cuenta'>
+                    Mi Cuenta
+                </LinkButton>
+                <MenuList>Entrar</MenuList>
+                <MenuList>Abrir Cuenta Nueva</MenuList>
+                </Menu>
+                <LinkButton to='/Preguntas Frecuentes'>
+                Preguntas Frecuentes
+                </LinkButton>
                 
                 </Box>
 
@@ -32,9 +64,8 @@ function App() {
                 {/* iconos */}
                 </Box>
             </Toolbar>
-        </Appbar>
+        </AppBar>
     );
-    </>
   }
 
-  export default Nav
+  export default Nav;
