51e4087c6b25c43386ee4b888c9ba9edc2dcc70f
Jonatan <jonatanchopp22@gmail.com>
2022-03-21 23:10:06 -0300

    Se agrego Marca con Imagen y Iconos de Search, Contacto y Cart

--- src/Componente/Nav.js
+++ src/Componente/Nav.js
@@ -1,76 +1,96 @@
-import React, {useState,} from 'react';
-import {AppBar, Menu, MenuList, Toolbar, Box, BottomNavigation, IconButton} from '@mui/material';
+import React, { useState } from "react";
+import {
+  AppBar,
+  Menu,
+  MenuList,
+  Toolbar,
+  Box,
+  BottomNavigation,
+  IconButton,
+  Grid,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PageviewIcon from "@mui/icons-material/Pageview";
+import "./Nav.css";
 
+function Nav() {
+  const [dropdown, setDropdown] = useState(false);
 
+  const abrirCerrarDropdown = () => {
+    setDropdown(!dropdown);
+  };
 
+  const LinkButton = (props) => (
+    <BottomNavigation
+      color="secondary"
+      isOpen={dropdown}
+      toggle={abrirCerrarDropdown}
+      bool={true}
+      {...props}
+    />
+  );
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Grid container>
+          <Grid>
+            <Box className="Marca">
+            </Box>
+            <Box className="NombreMarca" >
+                <h1>Sound On</h1>
+              </Box>
+          </Grid>
+        </Grid>
+        <Box>
+          {/* Drop Dows */}
 
-function App() {
-    const [dropdown, setDropdown]=useState(false);
+          <IconButton
+            edge="start"
+            color="inherit"
+            arial-label="menu"
+          ></IconButton>
+          <header>
+            <LinkButton to="/">Home</LinkButton>
 
-    const abrirCerrarDropdown=()=>{
-      setDropdown (!dropdown);
-    }
-  
-    const LinkButton = (props) => <BottomNavigation color='primary' isOpen={dropdown} toggle={abrirCerrarDropdown} bool={true} {...props} /> 
-    return (
-        <AppBar position= 'fixed'>
-            <Toolbar>
-                <Box>
-                <img src='C:\Users\Jonatan\proyectoReact\public\speaker-with-three-sound-waves-sound-emoji-11563014782dp23c5uqq3.png'alt='Simbolo Marca' ></img>
-                <h1>Sound On</h1>  
-                </Box>
-                <Box>
-                {/* Drop Dows */}
-    
-                <IconButton edge='start' color= 'inherit' arial-label= 'menu' ></IconButton>
-               <Menu>
-                <LinkButton to='/'>
-                    Home
-                </LinkButton>
-                </Menu>
+            <LinkButton to="/Categories">Categorias</LinkButton>
+            <Menu>
+              <MenuList>Guitarras/Bajos</MenuList>
+              <MenuList>Baterias</MenuList>
+              <MenuList>Piano</MenuList>
+              <MenuList>Violines</MenuList>
+            </Menu>
 
-                <Menu>
-                <LinkButton to='/Categories'>
-                    Categorias
-                </LinkButton>
-                <MenuList>Guitarras/Bajos</MenuList>
-                <MenuList>Baterias</MenuList>
-                <MenuList>Piano</MenuList>
-                <MenuList>Violines</MenuList>
-                </Menu>
+            <LinkButton to="/Sobre Nosotros">Sobre Nosotros</LinkButton>
+            <Menu>
+              <MenuList>Historia</MenuList>
+              <MenuList>Contacto</MenuList>
+            </Menu>
 
-                <Menu>
-                <LinkButton to='/Sobre Nosotros'>
-                    Sobre Nosotros
-                </LinkButton>
-                <MenuList>Historia</MenuList>
-                <MenuList>Contacto</MenuList>
-                </Menu>
+            <LinkButton to="/Mi Cuenta">Mi Cuenta</LinkButton>
+            <Menu>
+              <MenuList>Entrar</MenuList>
+              <MenuList>Abrir Cuenta Nueva</MenuList>
+            </Menu>
 
-                <Menu>
-                <LinkButton to='/Mi Cuenta'>
-                    Mi Cuenta
-                </LinkButton>
-                <MenuList>Entrar</MenuList>
-                <MenuList>Abrir Cuenta Nueva</MenuList>
-                </Menu>
-                <LinkButton to='/Preguntas Frecuentes'>
-                Preguntas Frecuentes
-                </LinkButton>
-                
-                </Box>
-                {/* iconos */}
-                <Box>
-                <img src='C:\Users\Jonatan\proyectoReact\public\1490129321-rounded10_82180 (2).png' alt='Search' ></img>
-                </Box>
-                <Box>
-                <img src='C:\Users\Jonatan\proyectoReact\public\1490129321-rounded10_82180 (2).png' alt='Search' ></img>
-                <img src='C:\Users\Jonatan\proyectoReact\public\round-account-button-with-user-inside_icon-icons.com_72596.png' alt='Users' ></img>
-                <img src='C:\Users\Jonatan\proyectoReact\public\1490129319-rounded09_82169.png' alt='Cart' ></img>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
-  }
+            <LinkButton to="/Preguntas Frecuentes">
+              Preguntas Frecuentes
+            </LinkButton>
+          </header>
+        </Box>
+        {/* iconos */}
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Box>
+              <AccountCircleIcon className="Users"></AccountCircleIcon>
+              <ShoppingCartIcon className="Cart"></ShoppingCartIcon>
+              <PageviewIcon className="Search"></PageviewIcon>
+            </Box>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
 
-  export default Nav;
+export default Nav;
