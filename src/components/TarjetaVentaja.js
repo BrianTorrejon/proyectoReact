bc637176e43e0af215b03ac4ccbaf45d241724ed
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-25 16:25:16 -0300

    Se posicionan tarjetas en componente Ventajas

--- src/components/TarjetaVentaja.js
+++ src/components/TarjetaVentaja.js
@@ -1,15 +1,16 @@
 import { Box, Grid, Paper, Typography } from '@material-ui/core'
 import React from 'react'
-import PianoIcon from '@mui/icons-material/Piano';
 
-const TarjetaVentaja = () => {
+const TarjetaVentaja = ({ icon, titulo, texto }) => {
     return (
         <>
-            <Box style={{ minHeight: '301px', width: '210px', }}>
-                <PianoIcon />
-                <Typography variant='h5'>Titulo</Typography>
-                <Typography variant='body1'>asdasdasd asdasdas dasdasdasdasda  asdasd asdasd asdasd  qweqwe </Typography>
+
+            <Box m={4} style={{ minHeight: '301px', width: '210px', }}> {/* m = 4  son 32 px */}
+                {icon}
+                <Typography variant='h5' >{titulo}</Typography>
+                <Typography variant='body1' >{texto}</Typography>
             </Box>
+
         </>
     )
 }
--- src/components/Ventajas.js
+++ src/components/Ventajas.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import TarjetaVentaja from './TarjetaVentaja'
+import PianoIcon from '@mui/icons-material/Piano';
+import { Box, Container, Grid } from '@material-ui/core';
 
-const Ventajas = () => { 
-  return (
-    <TarjetaVentaja/>
-  )
+const Ventajas = () => {
+    return (
+        <>
+            <Box style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <TarjetaVentaja
+                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
+                    titulo='Titulo'
+                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                <TarjetaVentaja
+                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
+                    titulo='Titulo'
+                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                <TarjetaVentaja
+                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
+                    titulo='Titulo'
+                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                <TarjetaVentaja
+                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
+                    titulo='Titulo'
+                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                <TarjetaVentaja
+                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
+                    titulo='Titulo'
+                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+            </Box>
+        </>
+
+
+    )
 }
 
 export default Ventajas
