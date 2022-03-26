664dfb535dc23ad041f1084af20ec242f0cd6a17
Jonatan <jonatanchopp22@gmail.com>
2022-03-26 09:03:32 -0300

    Se modificaron estilos y tamaños

--- src/Componente/Nav.js
+++ src/Componente/Nav.js
@@ -1,93 +1,148 @@
 import React, { useState } from "react";
 import {
   AppBar,
-  Menu,
-  MenuList,
   Toolbar,
   Box,
-  BottomNavigation,
+  Grid, 
   IconButton,
-  Grid,
-} from "@mui/material";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PageviewIcon from "@mui/icons-material/Pageview";
+  } from "@mui/material";
 import "./Nav.css";
+import { styled, alpha } from '@mui/material/styles';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+
 
 function Nav() {
-  const [dropdown, setDropdown] = useState(false);
-
-  const abrirCerrarDropdown = () => {
-    setDropdown(!dropdown);
+  const [anchorEl, setAnchorEl] = useState(true);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    };
+  const handleClose = () => {
+    setAnchorEl(false);
   };
 
-  const LinkButton = (props) => (
-    <BottomNavigation
-      color="secondary"
-      isOpen={dropdown}
-      toggle={abrirCerrarDropdown}
-      bool={true}
-      {...props}
-    />
-  );
+  const LinkButton = (props) =>
+      <Button
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        {...props} />
   return (
-    <AppBar position="fixed">
-      <Toolbar>
+    <AppBar className="Navbar">
+      <Toolbar className="config">
         <Grid container>
           <Grid>
-            <Box className="Marca">
-            </Box>
-            <Box className="NombreMarca" >
-                <h1>Sound On</h1>
-              </Box>
+            <Box className="Marca"></Box>
+            <Box className="NombreMarca"><h1><b>Sound On</b></h1></Box>
           </Grid>
         </Grid>
-        <Box>
+        <Grid>
           {/* Drop Dows */}
-
+          <Box>
           <IconButton
-            edge="start"
-            color="inherit"
-            arial-label="menu"
-          ></IconButton>
-          <header>
-            <LinkButton to="/">Home</LinkButton>
-
-            <LinkButton to="/Categories">Categorias</LinkButton>
-            <Menu>
-              <MenuList>Guitarras/Bajos</MenuList>
-              <MenuList>Baterias</MenuList>
-              <MenuList>Piano</MenuList>
-              <MenuList>Violines</MenuList>
-            </Menu>
-
-            <LinkButton to="/Sobre Nosotros">Sobre Nosotros</LinkButton>
-            <Menu>
-              <MenuList>Historia</MenuList>
-              <MenuList>Contacto</MenuList>
-            </Menu>
-
-            <LinkButton to="/Mi Cuenta">Mi Cuenta</LinkButton>
-            <Menu>
-              <MenuList>Entrar</MenuList>
-              <MenuList>Abrir Cuenta Nueva</MenuList>
-            </Menu>
-
-            <LinkButton to="/Preguntas Frecuentes">
-              Preguntas Frecuentes
-            </LinkButton>
-          </header>
-        </Box>
-        {/* iconos */}
-        <Grid container>
-          <Grid item xs={12} sm={6}>
-            <Box>
-              <AccountCircleIcon className="Users"></AccountCircleIcon>
-              <ShoppingCartIcon className="Cart"></ShoppingCartIcon>
-              <PageviewIcon className="Search"></PageviewIcon>
-            </Box>
-          </Grid>
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleClose}
+              color="inherit"
+            >
+            </IconButton>
+          </Box>
         </Grid>
+        <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}>
+         <Grid container spacing={2} columns={48}>
+              <Grid item xs="auto"> 
+            <LinkButton to="/">Home</LinkButton>
+              </Grid>
+              <Grid item xs={8}>         
+            <LinkButton to="/Categories">Categorias</LinkButton>
+              <MenuItem onClick={handleClose} disableRipple>Guitarras/Bajos</MenuItem>           
+              <MenuItem onClick={handleClose} disableRipple>Baterias</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Piano/Teclado</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Violines</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Parlantes</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Microfonos</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Auriculares</MenuItem>
+              </Grid>
+              <Grid item xs={12}>  
+            <LinkButton to="/Sobre Nosotros">Sobre Nosotros</LinkButton>
+              <MenuItem onClick={handleClose} disableRipple>Historia</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Contacto</MenuItem>
+              </Grid>
+              <Grid item xs={8}>  
+            <LinkButton to="/Mi Cuenta">Mi Cuenta</LinkButton>
+              <MenuItem onClick={handleClose} disableRipple>Entrar</MenuItem>
+              <MenuItem onClick={handleClose} disableRipple>Abrir Cuenta Nueva</MenuItem>
+              </Grid>
+              <Grid item xs> 
+            <LinkButton to="/Preguntas Frecuentes">Preguntas Frecuentes</LinkButton>
+              </Grid>
+         </Grid>
+    </StyledMenu>
+        {/* iconos */}
+          <Grid container className="Herramientas" direction="row" justifyContent="flex-end">
+            <Box className="Search"></Box>
+            <Box className="Users"></Box>
+            <Box className="Cart"></Box>
+            </Grid>
       </Toolbar>
     </AppBar>
   );
