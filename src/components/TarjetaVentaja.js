93008059780e6b1ef5911a93c2f5ed8be40b05ac
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-26 16:03:24 -0300

    Se agrega contenido a las tarjetas

--- src/components/TarjetaVentaja.js
+++ src/components/TarjetaVentaja.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, Typography } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import React from 'react'
 
 const TarjetaVentaja = ({ icon, titulo, texto }) => {
--- src/components/Ventajas.js
+++ src/components/Ventajas.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import TarjetaVentaja from './TarjetaVentaja'
 import PianoIcon from '@mui/icons-material/Piano';
-import { Box, Container, Grid } from '@material-ui/core';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
+import WorkspacePremiumOutlinedIcon from '@mui/icons-material/WorkspacePremiumOutlined';
+import { Box } from '@material-ui/core';
 
 const Ventajas = () => {
     return (
@@ -9,28 +13,26 @@ const Ventajas = () => {
             <Box style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
                 <TarjetaVentaja
                     icon={<PianoIcon sx={{ fontSize: 48 }} />}
-                    titulo='Titulo'
-                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                    titulo='Variedad'
+                    texto='Gran variedad de intrumentos musicales de todas partes del mundo, dede lo mas convencional hasta lo mas insolito.' />
                 <TarjetaVentaja
-                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
-                    titulo='Titulo'
-                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                    icon={<MusicNoteIcon sx={{ fontSize: 48 }} />}
+                    titulo='¿Cómo suena?'
+                    texto='Podras hacer prueba de sonido de cualquier instrumento sin compromiso o podras solicitar un video en caso de ser de otra ciudad.' />
                 <TarjetaVentaja
-                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
-                    titulo='Titulo'
-                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                    icon={<LocalShippingOutlinedIcon sx={{ fontSize: 48 }} />}
+                    titulo='Envios gratis'
+                    texto='Con cualquier compra que realices, el envio es gratis a cualquier punto del pais. Y si el monto supera los $15.000 se agregan regalos.' />
                 <TarjetaVentaja
-                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
-                    titulo='Titulo'
-                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                    icon={<LocalOfferOutlinedIcon sx={{ fontSize: 48 }} />}
+                    titulo='Descuentos exclusivos'
+                    texto='Tendras descuentos exclusivos en tu segunda compra que realices.' />
                 <TarjetaVentaja
-                    icon={<PianoIcon sx={{ fontSize: 48 }} />}
-                    titulo='Titulo'
-                    texto='asdd asdd asdd asdd adasdddas  asddddda  asddda asdsdsa asdd' />
+                    icon={<WorkspacePremiumOutlinedIcon sx={{ fontSize: 48 }} />}
+                    titulo='Calidad'
+                    texto='Contamos con un staff 100% calificado, y aseguramos la calidad de los instrumentos y sus respectivos accesorios' />
             </Box>
         </>
-
-
     )
 }
 
