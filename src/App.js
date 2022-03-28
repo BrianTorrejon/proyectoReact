d0164e7f007c52ab463bf6ea825b5855319ecac9
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-28 12:07:14 -0300

    Se crea rama mision

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { CssBaseline } from "@material-ui/core";
-import Banner from "./components/Banner";
+import Mision from "./components/Mision";
 
 
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Banner/>
+      <Mision/>
     </>
 
   );
---
+++ src/components/Mision.js
@@ -0,0 +1,16 @@
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    
+})
+
+const Mision = () => {
+
+    const classes = useStyles();
+    return (
+        <div>Mision</div>
+    )
+}
+
+export default Mision
