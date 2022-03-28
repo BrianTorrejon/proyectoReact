22b58136915d8a42bea837b2cd414abcd7ad9997
BrianTorrejon <briantorrejon@hotmail.com>
2022-03-28 19:02:38 -0300

    Se dio estilos y se agrega imagen

--- src/components/Mision.js
+++ src/components/Mision.js
@@ -1,15 +1,46 @@
 
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Box, Button, Grid, List, ListItem, ListItemIcon, ListItemText, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const useStyles = makeStyles({
     contenedor: {
-        marginLeft: '5%',
-        marginRight: '5%',
-        height: 487,
+        paddingLeft: '4%',
+        paddingRight: '4%',
+        maxHeight: 487,
+        width: '100%',
+        fontSize: 16,
+        '& .MuiSvgIcon-root': {
+            color: '#503BD9'
+        },
+        '& .MuiListItem-root': {
+            padding: 10
+        }
+    },
+    titulo: {
+        fontSize: 40
+    },
+    grid:{
+        minWidth:350
+    },
+    imagen: {
+        backgroundImage: 'url(https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: 570,
         width: '100%'
     },
-    grid: {
+    span: {
+        color: '#503BD9'
+    },
+    boton: {
+        color: '#ffffff',
+        backgroundColor: '#503BD9',
+        height: '45px',
+        fontSize: '14px',
+        '&:hover': {
+            backgroundColor: '#4230B3'
+        }
     }
 })
 
@@ -20,24 +51,52 @@ const Mision = () => {
     return (
 
 
-        <Grid container className={classes.contenedor}>
-            <Grid item className={classes.grid} xs={12} sm={6}>
-                <Typography variant='h4'>Quienes somos</Typography>
-                <Typography variant="body1">
-                    Nuesta mision es conenctar a las personas con el mundo de la musica a traves de los diversos intrumentos musicales.
-                    Por que?
+        <Grid container className={classes.contenedor} spacing={8}>
+            <Grid item xs={12} sm={6} className={classes.grid}>
+                <Typography variant='h4' className={classes.titulo}><span className={classes.span}>¿Quieres formar parte</span> del mundo de la musica?</Typography>
+                <Typography component='div' >
+                    <Box sx={{ fontWeight: 'bold' }}>
+                        Nuesta mision es conenctar a las personas con el mundo de la musica a traves de los diversos intrumentos musicales.
+                        ¿Por que?
+                    </Box>
                 </Typography>
-                <ul>
-                    <li>asdasd</li>
-                    <li>asdasd</li>
-                    <li>asdasd</li>
-                    <li>asdasd</li>
-                    <li>asdasd</li>
-                </ul>
-                <Button color="primary">Descubre nuestos intrumentos</Button>
+                <List>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CheckCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Tocar un instrumento incita a desarrollar la creatividad." />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CheckCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Ayuda al bienestar de la vida diaria, mejorando tu estado de animo." />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CheckCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Fomenta la facilidad de adaptacion a cambios constantes." />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CheckCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Es un buen ejercicio para el desarrollo cerebral y la memoria." />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <CheckCircleIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Mayor disciplina y paciencia." />
+                    </ListItem>
+                </List>
+
+                <Button className={classes.boton}>Descubre nuestos intrumentos</Button>
             </Grid>
-            <Grid item className={classes.grid} xs={12} sm={6}>
-                Imagen
+            <Grid item className={classes.imagen} xs={12} sm={6} >
+
             </Grid>
         </Grid>
 
